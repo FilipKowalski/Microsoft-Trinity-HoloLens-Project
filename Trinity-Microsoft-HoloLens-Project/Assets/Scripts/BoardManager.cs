7acdb449fc31d758de2ed36967c6a583bb372cd7
Filip Kowalski <55386371+FilipKowalski@users.noreply.github.com>
2022-03-08 15:21:08 +0000

    Triggers and 2d array (#4)
    * added triggers and 2d array
    
    * Need to debug trigger enter and exit
    
    * changed origin of triggers and added new trigger manager script for pieces
    
    * updateArray Code Added
    
    * Array changes when piece moved to represent state of the board

--- Trinity-Microsoft-HoloLens-Project/Assets/Scripts/BoardManager.cs
+++ Trinity-Microsoft-HoloLens-Project/Assets/Scripts/BoardManager.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
+
+    #region ChessPieceEnumeration
+
+    private int EMPTY_SPACE = -1;
+    private int BLACK_KING = 0;
+    private int BLACK_QUEEN = 1;
+    private int BLACK_ROOK = 2;
+    private int BLACK_BISHOP = 3;
+    private int BLACK_KNIGHT = 4;
+    private int BLACK_PAWN = 5;
+    private int WHITE_KING = 6;
+    private int WHITE_QUEEN = 7;
+    private int WHITE_ROOK = 8;
+    private int WHITE_BISHOP = 9;
+    private int WHITE_KNIGHT = 10;
+    private int WHITE_PAWN = 11;
+
+    #endregion ChessPieceEnumeration
+
     //if one unit is 1 metre lets make the tiles 8cms for now
-    private const float TILE_SIZE = 0.08f;
-    private const float TILE_OFFSET = 0.04f;
+    private const float TILE_OFFSET = 0.76243f;
+    private const float TILE_SIZE = 1.52486f;
     private bool collide = true;
+
+    private const int CHESSBOARD_SIZE = 8;
+    private  int[,] chessBoard = new int[CHESSBOARD_SIZE, CHESSBOARD_SIZE];
 
     public List<GameObject> chessPiecePrefabs;
     public List<GameObject> activeChessPieces;
@@ -21,15 +43,42 @@ public class BoardManager : MonoBehaviour
 
     private void Start()
     {
+        InitialiseArray();
     }
 
+    public void updateArray(Vector3 oldPos, Vector3 newPos)
+    {
+        Debug.Log("WORKING!");
+        //get the indexes
+        int oldX = (int) ((oldPos.x - TILE_OFFSET) / TILE_SIZE + 0.5);
+        int oldY = (int) ((oldPos.z - TILE_OFFSET) / TILE_SIZE + 0.5);
+
+        int newX = (int) ((newPos.x - TILE_OFFSET) / TILE_SIZE + 0.5);
+        int newY = (int) ((newPos.z - TILE_OFFSET) / TILE_SIZE + 0.5);
+
+        chessBoard[newX, newY] = chessBoard[oldX, oldY];
+        chessBoard[oldX, oldY] = EMPTY_SPACE;
+
+        //for debugging the array
+        /*
+        string debug = "";
+        for (int i = 0; i < 8; i++)
+        {
+            debug += "{";
+            for (int j = 0; j < 8; j++)
+            {
+                debug += chessBoard[i, j] + ", ";
+            }
+            debug += "} \n";
+        }
+        */
+    }
 
     public void ToggleKinematic()
     {
         foreach (Rigidbody piece in ChessRigidBodies)
             piece.isKinematic = !piece.isKinematic;
     }
-
 
     public void ToggleCollider()
     {
@@ -40,5 +89,69 @@ public class BoardManager : MonoBehaviour
     public void tableFlip()
     {
         board.AddExplosionForce(10f, new Vector3(10f, 10f), 2);
+    }
+
+    public void InitialiseArray()
+    {
+        //initialise array
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                chessBoard[i, j] = EMPTY_SPACE;
+            }
+        }
+
+        //White Chesspieces
+
+        //king
+        chessBoard[3, 0] = WHITE_KING;
+
+        //Queen
+        chessBoard[4, 0] = WHITE_QUEEN;
+
+        //Rooks
+        chessBoard[0, 0] = WHITE_ROOK;
+        chessBoard[7, 0] = WHITE_ROOK;
+
+        //Bishops
+        chessBoard[2, 0] = WHITE_BISHOP;
+        chessBoard[5, 0] = WHITE_BISHOP;
+
+        //Knights
+        chessBoard[1, 0] = WHITE_KNIGHT;
+        chessBoard[6, 0] = WHITE_KNIGHT;
+
+        //Pawns
+        for (int i = 0; i < 8; i++)
+        {
+            chessBoard[i, 1] = WHITE_PAWN;
+        }
+
+        //Black chesspieces
+
+        //king
+        chessBoard[3, 7] = BLACK_KING;
+
+        //Queen
+        chessBoard[4, 7] = BLACK_QUEEN;
+
+        //Rooks
+        chessBoard[0, 7] = BLACK_ROOK;
+        chessBoard[7, 7] = BLACK_ROOK;
+
+        //Bishops
+        chessBoard[2, 7] = BLACK_BISHOP;
+        chessBoard[5, 7] = BLACK_BISHOP;
+
+        //Knights
+        chessBoard[1, 7] = BLACK_KNIGHT;
+        chessBoard[6, 7] = BLACK_KNIGHT;
+
+        //Pawns
+        for (int i = 0; i < 8; i++)
+        {
+            chessBoard[i, 6] = BLACK_PAWN;
+        }
     }
 }
---
+++ Trinity-Microsoft-HoloLens-Project/Assets/Scripts/PieceTriggerManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceTriggerManager : MonoBehaviour
+{
+    BoardManager boardManager;
+    Vector3 oldPos = Vector3.zero;
+    Vector3 newPos;
+
+    void Awake()
+    {
+        boardManager = GameObject.Find("GameBoard").GetComponent<BoardManager>();
+    }
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        Debug.Log("ENTER");
+        newPos = trigger.transform.localPosition;
+        if (oldPos != Vector3.zero && oldPos != newPos)
+            boardManager.updateArray(oldPos, newPos);
+    }
+
+    private void OnTriggerExit(Collider trigger)
+    {
+        Debug.Log("EXIT");
+        oldPos = trigger.transform.localPosition;
+    }
+}
